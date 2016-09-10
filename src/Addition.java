16866016b1aaf9535b347b6daa97fdb26fcd8876
jvirk13 <300869382@cencol.ca>
2016-09-09 20:10:17 -0400

    initial commit

---
+++ src/Addition.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+         int num1;
+         int num2;
+         int sum;
+         System.out.print("Enter the first integer: ");
+         num1=input.nextInt();
+         System.out.print("Enter the second integer: ");
+         num2=input.nextInt();
+         
+         sum=num1+num2;
+         
+         System.out.printf("sum is %d%n",sum);
+	}
+
+}
+
